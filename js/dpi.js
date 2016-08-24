688d3ec6dcde92f9279ae4fd89b690245203a4cd
Attila Földes <foeldes@ionauts.com>
2016-08-24 15:27:47 +0200

    customizable jaw and windows multiplier

--- js/dpi.js
+++ js/dpi.js
@@ -1,6 +1,3 @@
-var WINDOWS = 1;
-var JAW = 0.022;
-
 function round(value, decimals) {
   let precision = Math.pow(10, decimals);
   return Number(Math.round(value * precision) / precision);
@@ -10,10 +7,12 @@ function calc() {
   let cdpi = document.getElementById("cdpi").value;
   let tdpi = document.getElementById("tdpi").value;
   let sens = document.getElementById("sens").value;
+  let jaw = document.getElementById("jaw").value;
+  let windows = document.getElementById("windows").value;
 
-  let result = sens/(tdpi/cdpi) * WINDOWS;
+  let result = sens/(tdpi/cdpi) * windows;
   let edpi = tdpi * result;
-  let cm180  = ((180 / JAW) / edpi) * 2.54;
+  let cm180  = ((180 / jaw) / edpi) * 2.54;
 
   document.getElementById("result").value = round(result, 2);
   document.getElementById("edpi").value = round(edpi, 0);
